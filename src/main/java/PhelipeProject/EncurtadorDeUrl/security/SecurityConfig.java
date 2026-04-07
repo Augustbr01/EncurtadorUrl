4da6eb8191e9dac8a2a78a25d3f26d2cf24481dc
phelipegit <phelipegithub@gmail.com>
2026-04-07 01:24:45 -0300

    Cors atualizado

--- src/main/java/PhelipeProject/EncurtadorDeUrl/security/SecurityConfig.java
+++ src/main/java/PhelipeProject/EncurtadorDeUrl/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://phelipegitencurtador.vercel.app"));
+        config.setAllowedOriginPatterns(List.of("https://*.vercel.app", "http://localhost:*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
